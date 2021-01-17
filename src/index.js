3669dbd7842fd94f22d612d0bf3501ce0661f30b
sheerangel <77532755+sheerangel@users.noreply.github.com>
2021-01-17 16:01:45 +0530

    Added new features

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "SHEER";
+const lname = "ANGEL";
+const num = 0;
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fname} ${lname}`}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
